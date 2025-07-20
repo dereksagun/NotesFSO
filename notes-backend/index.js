3cc4f6e2d19356c8710a14c38ab178aeb9160c97
Derek Sagun <dereksagun@Dereks-MacBook-Pro.local>
2025-07-20 15:13:51 -0700

    uibuild

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -31,9 +31,9 @@ app.get('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   const id = request.params.id
-
+  
   Note.findByIdAndDelete(id)
     .then(result => {
       response.status(204).end()
@@ -49,7 +49,7 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
     if(!body.content){
       return response.status(400).json({
@@ -65,7 +65,7 @@ app.post('/api/notes', (request, response) => {
 
     note.save().then(savedNote => {
       response.json(savedNote)
-    })
+    }).catch(error => next(error))
   
 })
 
@@ -100,7 +100,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }  else if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
